05301b9fed707cede939b98017248db7ecd5f164
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2024-11-18 17:29:40 +0900

    bird

--- json-schema.ts
+++ json-schema.ts
@@ -1,50 +1,52 @@
-import AnthropicBedrock from '@anthropic-ai/bedrock-sdk';
+import Anthropic from '@anthropic-ai/sdk';
 import { Tool } from '@anthropic-ai/sdk/resources/messages.mjs';
 import fs from "fs";
 import { z } from "zod";
 import zodToJsonSchema from 'zod-to-json-schema';
 
-const client = new AnthropicBedrock({ awsRegion: 'us-east-1' });
+const client = new Anthropic();
 
 const schema = z.object({
-  title: z.string(),
-  description: z.string(),
-  salary_currency: z.enum(["USD", "EUR", "JPY"]),
-  salary_min: z.number().optional(),
-  salary_max: z.number().optional(),
-  salary_note: z.string().optional(),
-  location: z.string(),
-  employment_type: z.enum(["full-time", "part-time", "contract", "temporary", "internship"]),
-  benefits: z.array(z.string()),
+  commonName: z.string(),
+  scientificName: z.string(),
+  length: z.number(),
+  wingspan: z.number(),
+  weight: z.number(),
+  class: z.string(),
+  order: z.string(),
+  family: z.string(),
+  genus: z.string(),
+  species: z.string(),
+  summary: z.string().describe("A brief summary of the species"),
+  feeding_habit: z.enum(["herbivore", "carnivore", "omnivore", "insectivore", "frugivore", "nectarivore", "molluscivore", "piscivore", "detritivore"]).array(),
+  distribution: z.string().array(),
 });
 
-function zodToInputSchema<T>(
-  schema: z.ZodSchema<T>
-): Tool.InputSchema {
+function zodToInputSchema<T>(schema: z.ZodSchema<T>): Tool.InputSchema {
   const jsonSchema = zodToJsonSchema(schema, "input").definitions?.input;
   if (!jsonSchema) {
     throw new Error("Failed to convert Zod schema to JSON schema");
   }
-  console.log(jsonSchema);
   return jsonSchema as Tool.InputSchema;
 }
 
-const content = fs.readFileSync(process.stdin.fd, "utf-8");
+const content = fs.readFileSync(process.stdin.fd, 'utf-8');
 
 const response = await client.messages.create({
-  model: "anthropic.claude-3-5-sonnet-20240620-v1:0",
-  temperature: 0.1,
+  model: "claude-3-5-haiku-latest",
+  temperature: 0,
   max_tokens: 8000,
   tool_choice: {
     type: "tool",
     name: "json-output",
   },
-  system: "Convert the given text into structured data",
+  system: "Convert the given text into structured data using json-output tool",
   messages: [{role: "user", content}],
   tools: [
     {
       name: "json-output",
       input_schema: zodToInputSchema(schema),
+      description: "Export structured data as JSON",
     }
   ],
 });
